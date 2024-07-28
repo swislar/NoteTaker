37d48a398b9733d10496bf35bb09ca4bbaea931d
happlesful <Swislar1108@gmail.com>
2024-07-28 10:05:34 +0800

    Added: Statify menu component

--- client/src/app/components/index.ts
+++ client/src/app/components/index.ts
@@ -3,3 +3,5 @@ export { UsercardComponent } from './usercard/usercard.component';
 export { NoteComponent } from './note/note.component';
 export { SpotifyProfileComponent } from './spotify-profile/spotify-profile.component';
 export { HeaderComponent } from './header/header.component';
+export { SliderComponent } from './slider/slider.component';
+export { StatifyMenuComponent } from './statify-menu/statify-menu.component';
